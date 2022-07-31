97c287e372cf0a828e20bbdb7405f248e51e5b89
Vivek-Kumar-Singh135 <vivekkrsingh135@gmail.com>
2022-07-31 17:57:42 +0530

    commit1

--- index.js
+++ index.js
@@ -1,28 +1,45 @@
 class SortedList {
-  constructor() {
-  
-  }
-  add() {
-   
-  }
-  get() {
+    constructor() {
+        this.items = []
+        this.length = 0;
+    }
+    add(item) {
+        this.items.push(item)
+        this.items.sort((a, b) => { return a - b });
+        this.length = this.length + 1;
+    }
+    get(idx) {
+        if (idx < 0 || idx > this.length - 1) throw new Error('OutOfBounds');
+        else return this.items[idx];
+    }
+    max() {
+        if (this.length === 0) throw new Error('EmptySortedList')
+        else {
+            let max = Math.max(...this.items);
+            return max;
+        }
+    }
+    min() {
+        if (this.length === 0) throw new Error('EmptySortedList')
+        else {
+            let min = Math.min(...this.items);
+            return min;
+        }
+    }
+    avg() {
+        if (this.length === 0) throw new Error('EmptySortedList')
+        else {
+            let val = this.items.reduce((item, total) => { return item + total }, 0);
+            return val / this.length;
+        }
 
-  }
-  max() {
+    }
 
-  }
-  min() {
+    sum() {
+        let val = this.items.reduce((item, total) => { return item + total }, 0);
+        return val;
+    }
 
-  }
-  avg() {
-    
-   
-  }
-
-  sum() {
-  
-  }
-  
 };
 
 module.exports = SortedList;
